82d6662f27a1f37538593a475ee87230c665b657
shiyangwang001 <xiaoyang248625@126.com>
2016-04-01 19:17:59 +0800

    dddd

--- src/com/ddmap/model/Text.java
+++ src/com/ddmap/model/Text.java
@@ -3,7 +3,7 @@ package com.ddmap.model;
 public class Text {
 	public static void main(String[] args) {
         System.out.print("");
-        System.out.print("");
+        System.out.print("ddddd");
 	}
 
 }
